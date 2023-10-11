07ea5eb9ffa894f9075cef014fac2342511fd5e6
Eslam Gohar <islamgoher1@gmail.com>
2023-10-11 18:09:15 +0300

    Applying Pagination and Infinite Loading by SWR data fetching lib.

---
+++ client/src/components/HomeSWR.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useSWR from "swr";
+
+const HomeSWR = () => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR("http://localhost:3001", fetcher);
+
+  if (error) return <div>Failed to fetch users.</div>;
+  if (isLoading) return <h2>Loading...</h2>;
+
+  return (
+    <>
+      <div>
+        {data.map((user, index) => {
+          return <h2 key={index}>{user.name}</h2>;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default HomeSWR;
